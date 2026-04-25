9f0afb0e0390696026f09f89b2249f7860820694
Kgsservice <info@kgs-lao.com>
2026-04-25 08:15:09 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const V = 'bizpro-v2';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(() => new Response('Offline'))); });
